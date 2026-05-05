ab3a25a96ccdb4c944046eb4e23919953de2bae5
Clement <clementcrownrise@gmail.com>
2026-05-05 16:39:43 +0100

    sesa

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from faculty.models import Faculty
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, first_name, last_name, username, email, password=None):
+    def create_user(self, email, username,first_name, last_name,  password=None):
         if not email:
             raise ValueError('User must have an email address')
         
@@ -22,7 +22,7 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, first_name, last_name, username, email, password=None):
+    def create_superuser(self, email, username, first_name, last_name,  password=None):
         user = self.create_user(
             email =self.normalize_email(email),
             username = username, 
